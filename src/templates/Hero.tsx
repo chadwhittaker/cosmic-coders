b7f0d28dae6c5c518136da71e791b45b81bb382d
cwhittaker <cwhittaker@cainc.com>
2023-11-08 21:36:21 -0500

    add text and email input

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -4,40 +4,97 @@ import { Section } from '../layout/Section';
 import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
 import { Logo } from './Logo';
 
-const Hero = () => (
-  <Background color="bg-gray-100">
-    <Section yPadding="py-6">
-      <NavbarTwoColumns logo={<Logo xl />}>
-        <div></div>
-        <div></div>
-      </NavbarTwoColumns>
-    </Section>
+const Hero = () => {
+  const onSubmit = () => {
+    // console.log('submit');
+  };
 
-    <Section yPadding="pt-20 pb-32">
-      <HeroOneButton
-        title={
-          <>
-            {'Apps, websites, and more\n'}
-            <span className="text-primary-500">
-              Our software is out of this planet
-            </span>
-          </>
-        }
-        description="If you are interested in working with us please provide your email"
-        button={
-          <div>
-            <input
-              type="text"
-              id="first_name"
-              className="w-[300px] rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-              placeholder="Email..."
-              required
-            ></input>
-          </div>
-        }
-      />
-    </Section>
-  </Background>
-);
+  return (
+    <Background color="bg-gray-100">
+      <Section yPadding="py-6">
+        <NavbarTwoColumns logo={<Logo xl />}>
+          <div></div>
+          <div></div>
+        </NavbarTwoColumns>
+      </Section>
+
+      <Section yPadding="pt-20 pb-32">
+        <HeroOneButton
+          title={
+            <>
+              {'Apps, websites, and more\n'}
+              <span className="text-primary-500">
+                Our software is out of this planet
+              </span>
+            </>
+          }
+          description="If you are interested in working with us please provide your email"
+          button={
+            <div className="flex justify-center">
+              <form className="w-[500px]">
+                <label
+                  htmlFor="search"
+                  className="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Search
+                </label>
+                <div className="relative">
+                  <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                    <svg
+                      className="h-6 w-6 text-gray-300 dark:text-gray-400"
+                      width="800px"
+                      height="800px"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M3.75 5.25L3 6V18L3.75 18.75H20.25L21 18V6L20.25 5.25H3.75ZM4.5 7.6955V17.25H19.5V7.69525L11.9999 14.5136L4.5 7.6955ZM18.3099 6.75H5.68986L11.9999 12.4864L18.3099 6.75Z"
+                        fill="#888"
+                      />
+                    </svg>
+                    {/* <svg
+                    className="h-4 w-4 text-gray-500 dark:text-gray-400"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                    />
+                  </svg> */}
+                  </div>
+                  <input
+                    type="email"
+                    id="email"
+                    className="block w-full rounded-lg border border-gray-500 bg-gray-50 p-4 pl-10 text-sm text-gray-900 placeholder:text-gray-600 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                    placeholder="Your email here..."
+                    required
+                  />
+                  <button
+                    onClick={onSubmit}
+                    type="submit"
+                    className="absolute bottom-2.5 right-2.5 rounded-lg bg-blue-700 px-4 py-2 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Send
+                  </button>
+                </div>
+              </form>
+            </div>
+          }
+        />
+      </Section>
+    </Background>
+  );
+};
 
 export { Hero };
--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -3,26 +3,26 @@ import { Section } from '../layout/Section';
 
 const VerticalFeatures = () => (
   <Section
-    title="Your title here"
-    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus malesuada nisi tellus, non imperdiet nisi tempor at."
+    title="Software design & engineering simplified"
+    description="Do you have an idea for a software product but don't know where to start? We can help with every step of the process from concept, to design, to engineering, and even maintenance."
   >
     <VerticalFeatureRow
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
+      title="Website Applications"
+      description="We can devliver robust and future proof website designs built on the most modern web frameworks with lightning fast response times. Our websites are easy to launch, update, and maintain."
       image="/assets/images/feature.svg"
       imageAlt="First feature alt text"
     />
     <VerticalFeatureRow
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
-      image="/assets/images/feature2.svg"
+      title="iOS & Android Apps"
+      description="Have an idea for an app? We can quickly turn your idea into an app in the iOS or Android app store."
+      image="/assets/images/feature3.svg"
       imageAlt="Second feature alt text"
       reverse
     />
     <VerticalFeatureRow
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
-      image="/assets/images/feature3.svg"
+      title="SEO, Marketing, and Analytics"
+      description="The job doesn't stop when your software is launched. Work with one of our marketing teams to fine tune your Seach Engine Optimization and get analytics on the performance of your software."
+      image="/assets/images/feature2.svg"
       imageAlt="Third feature alt text"
     />
   </Section>
